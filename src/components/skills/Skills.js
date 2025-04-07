87b4248d90bea2b4412a82bcf97da2824ea5cbcf
Woolf <franjaresc@hotmail.com>
2025-04-07 20:32:53 +0200

    Change the primary foreground color light

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -2,30 +2,30 @@ import Icons from "@/components/icons/Icons";
 import Image from "next/image";
 
 const TechnicalSkills = [
-  { id: 0, nombre: "Java", Icon: Icons.Java, label: "Java", color: "text-primary" },
-  { id: 1, nombre: "Python", Icon: Icons.Python, label: "Python", color: "text-primary" },
-  { id: 2, nombre: "HTML5", Icon: Icons.HTML, label: "HTML5", color: "text-primary" },
-  { id: 3, nombre: "CSS", Icon: Icons.CSS, label: "CSS3", color: "text-primary" },
-  { id: 4, nombre: "JavaScript", Icon: Icons.JavaScript, label: "JavaScript", color: "text-primary" },
-  { id: 5, nombre: "React", Icon: Icons.React, label: "React", color: "text-primary" },
-  { id: 6, nombre: "NodeJS", Icon: Icons.NodeJS, label: "NodeJS", color: "text-primary" },
-  { id: 7, nombre: "Express", Icon: Icons.Express, label: "Express", color: "text-primary" },
-  { id: 8, nombre: "MongoDB", Icon: Icons.MongoDB, label: "MongoDB", color: "text-primary" },
-  { id: 9, nombre: "Docker", Icon: Icons.Docker, label: "Docker", color: "text-primary" },
-  { id: 10, nombre: "Git", Icon: Icons.Git, label: "Git", color: "text-primary" },
-  { id: 11, nombre: "GitHub", Icon: Icons.GitHub, label: "GitHub", color: "text-primary" },
-  { id: 12, nombre: "Azure", Icon: Icons.Azure, label: "Azure", color: "text-primary" },
-  { id: 13, nombre: "SQL", Icon: Icons.SQL, label: "SQL", color: "text-primary" },
-  { id: 14, nombre: "NextJS", Icon: Icons.NextJS, label: "NextJS", color: "text-primary" },
+  { id: 0, nombre: "Java", Icon: Icons.Java, label: "Java", color: "text-primary-foregrond" },
+  { id: 1, nombre: "Python", Icon: Icons.Python, label: "Python", color: "text-primary-foregrond" },
+  { id: 2, nombre: "HTML5", Icon: Icons.HTML, label: "HTML5", color: "text-primary-foregrond" },
+  { id: 3, nombre: "CSS", Icon: Icons.CSS, label: "CSS3", color: "text-primary-foregrond" },
+  { id: 4, nombre: "JavaScript", Icon: Icons.JavaScript, label: "JavaScript", color: "text-primary-foregrond" },
+  { id: 5, nombre: "React", Icon: Icons.React, label: "React", color: "text-primary-foregrond" },
+  { id: 6, nombre: "NodeJS", Icon: Icons.NodeJS, label: "NodeJS", color: "text-primary-foregrond" },
+  { id: 7, nombre: "Express", Icon: Icons.Express, label: "Express", color: "text-primary-foregrond" },
+  { id: 8, nombre: "MongoDB", Icon: Icons.MongoDB, label: "MongoDB", color: "text-primary-foregrond" },
+  { id: 9, nombre: "Docker", Icon: Icons.Docker, label: "Docker", color: "text-primary-foregrond" },
+  { id: 10, nombre: "Git", Icon: Icons.Git, label: "Git", color: "text-primary-foregrond" },
+  { id: 11, nombre: "GitHub", Icon: Icons.GitHub, label: "GitHub", color: "text-primary-foregrond" },
+  { id: 12, nombre: "Azure", Icon: Icons.Azure, label: "Azure", color: "text-primary-foregrond" },
+  { id: 13, nombre: "SQL", Icon: Icons.SQL, label: "SQL", color: "text-primary-foregrond" },
+  { id: 14, nombre: "NextJS", Icon: Icons.NextJS, label: "NextJS", color: "text-primary-foregrond" },
 ];
 
 const InterpersonalSkills = [
-  { id: 0, nombre: "Teamwork", label: "Teamwork", color: "text-primary" },
-  { id: 1, nombre: "Leadership", label: "Leadership", color: "text-primary" },
-  { id: 2, nombre: "Communication", label: "Communication", color: "text-primary" },
-  { id: 3, nombre: "Problem Solving", label: "Problem Solving", color: "text-primary" },
-  { id: 4, nombre: "Adaptability", label: "Adaptability", color: "text-primary" },
-  { id: 5, nombre: "Creativity", label: "Creativity", color: "text-primary" },
+  { id: 0, nombre: "Teamwork", label: "Teamwork", color: "text-primary-foregrond" },
+  { id: 1, nombre: "Leadership", label: "Leadership", color: "text-primary-foregrond" },
+  { id: 2, nombre: "Communication", label: "Communication", color: "text-primary-foregrond" },
+  { id: 3, nombre: "Problem Solving", label: "Problem Solving", color: "text-primary-foregrond" },
+  { id: 4, nombre: "Adaptability", label: "Adaptability", color: "text-primary-foregrond" },
+  { id: 5, nombre: "Creativity", label: "Creativity", color: "text-primary-foregrond" },
 ];
 
 function Skills() {
